32f2fbf781aacf986c7bee7fe478ea6ca3d56550
sonupvlog8937 <sonupvlog8937@gmail.com>
2025-01-14 15:32:21 +0000

    hello

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-slate-200'>
       <div className='container mx-auto p-4'>
-       <p className='text-center font-bold' title="Youtube Channel">Contact Us +91 8969737537</p>
+       <p className='text-center font-bold' title="Youtube Channel">Contact Us +91737537</p>
       </div>
     </footer>
   )
