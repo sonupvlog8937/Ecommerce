89817e3314a13faf83589af255cbb7b8e11cf2d1
sonupvlog8937 <sonupvlog8937@gmail.com>
2025-01-15 13:51:38 +0000

    three

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
     <footer className='bg-slate-200'>
       <div className='container mx-auto p-4'>
        <p className='text-center font-bold' title="Youtube Channel">Contact Us +91 8969737537</p>
-       <p>this is sonu prajapati</p>
       </div>
     </footer>
   )
